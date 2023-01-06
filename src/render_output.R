cb3935060fa3027c57d97c89a89c27af95a31f85
baarthur <baarthur0@outlook.com>
2023-01-06 00:58:42 -0300

    ajustes

--- src/render_output.R
+++ src/render_output.R
@@ -5,3 +5,9 @@ quarto::quarto_render(
   output_format = "pdf",
   output_file = "output/Lista 1 de Economia Urbana"
 )
+
+rmarkdown::render(
+  input = here::here("src/2_notas de aula.Rmd"),
+  output_format = "pdf_document",
+  output_file = "Notas - Aula 12",
+  output_dir = here::here("output/"))
