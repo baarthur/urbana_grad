7fb0c10cfff4180790179b0aff569273d59cb410
baarthur <baarthur0@outlook.com>
2023-01-09 01:12:35 -0300

    slides_v2

--- src/render_output.R
+++ src/render_output.R
@@ -6,8 +6,15 @@ quarto::quarto_render(
   output_file = "output/Lista 1 de Economia Urbana"
 )
 
+
+rmarkdown::render(
+  input = here::here("index.Rmd"),
+  output_format = "xaringan::moon_reader",
+  output_file = "index.html")
+
+
 rmarkdown::render(
   input = here::here("src/2_notas de aula.Rmd"),
   output_format = "pdf_document",
-  output_file = "Notas - Aula 12",
+  output_file = "Notas - Aula 10 (semana 12).pdf",
   output_dir = here::here("output/"))
